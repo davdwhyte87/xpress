66bbd71ce9f8c47661c1ae7cef0ac2413c0f2423
vhenngames <37925345+davdwhyte87@users.noreply.github.com>
2023-07-15 00:07:31 +0100

    api setup

--- src/main/java/com/example/xpress/controllers/AuthController.java
+++ src/main/java/com/example/xpress/controllers/AuthController.java
@@ -1,13 +1,28 @@
 package com.example.xpress.controllers;
 
 
+
+
+
+import com.example.xpress.models.requests.LoginRequest;
+import com.example.xpress.utils.ResponseHandler;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class AuthController {
-    @GetMapping("/hello")
-    public String Hello(){
-        return "Hello bro";
+    @PostMapping("/hello")
+    public ResponseEntity<Object> Hello(@Valid @RequestBody LoginRequest loginRequest){
+
+        return ResponseHandler.responseBuilder("ok", HttpStatus.OK, loginRequest);
     }
 }
---
+++ src/main/java/com/example/xpress/models/requests/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.example.xpress.models.requests;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class LoginRequest {
+    @jakarta.persistence.Id
+    public String Id;
+    @NotBlank
+    public String UserName;
+    @NotBlank
+    public String Password;
+}
